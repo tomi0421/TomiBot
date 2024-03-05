70cfa0e7c6ab32f193e397671a4d741fcbc1a47b
tomi0421 <160614852+tomi0421@users.noreply.github.com>
2024-03-05 22:44:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.all()  
+bot = commands.Bot(command_prefix='t!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+#👇この下にあるやつがt!memberで動くはず
+@bot.command(name='member')
+async def member_info(ctx, member: discord.Member):
+    embed = discord.Embed(title=f'Member Information for {member.name}', color=member.color)
+
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.add_field(name='ID', value=member.id, inline=False)
+    embed.add_field(name='Top Role', value=member.top_role.name, inline=False)
+    embed.add_field(name='Joined Server', value=member.joined_at.strftime('%Y-%m-%d %H:%M:%S'), inline=False)
+
+    await ctx.send(embed=embed)
+
+bot.run('MTIxNDU2ODgzNzM5MTk4MjYzMw.GphZqB.xeriXkr7_5rDXdjb6ONwWvpRDMFIUGdJHzSFYo')
