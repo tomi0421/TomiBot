433941ee3e4c1861c4f6b8b7991d1a7ed02ce368
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-06 17:52:00 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import discord
+
+intents = discord.Intents.all()
+client = discord.Client(intents = intents)
+tree = discord.app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord. Activity(type=discord.ActivityType.competing, name=f'{len(client.guilds)}'))
+    print(f'{client.user} is online!')
+    await tree.sync()
+
+@tree.command(name="test", description="test command")
+async def panel_au(interaction: discord.Interaction):
+    await interaction.respons.send(“test”)
+
+cliente.run(os.getenv('token'))
