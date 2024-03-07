4ccd19dd7ae2d013b044067cf04008d299dd4300
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-07 15:17:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -19,34 +19,33 @@ async def embed_command(interaction:discord.Interaction,title:str,description:st
     await interaction.response.send_message(embed=embed)
   except Exception as e:
     print(e)
-    await interaction.response.send_message("エラーが発生しました", ephemeral=True)
+    await interaction.response.send_message("エラーが発生しました",ephemeral=True)
 
 @tree.command(name="userinfo", description="指定したユーザーの情報を表示する")
 async def user_info(interaction: discord.Interaction,user:discord.User):
   try:
-    embed = discord.Embed(title=ユーザー.display_name)
+    embed = discord.Embed(title=user.display_name)
     embed.set_thumbnail(
         url=user.avatar.url if user.avatar else discord.Embed.Empty)
 
-    embed.add_field(name="user name", value=user.name,inline=False)
-    embed.add_field(name="account create",
-                    value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"),inline=False)
+    embed.add_field(name="user name",value=user.name,inline=False)
+    embed.add_field(name="account create",value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"),inline=False)
 
     guild = interaction.guild
     member = guild.get_member(user.id)
     if member:
       roles = [role.mention for role in member.roles[1:]]
       if roles:
-        embed.add_field(name="role", value=" ".join(roles), inline=False)
+        embed.add_field(name="role",value=" ".join(roles),inline=False)
       else:
-        embed.add_field(name="role", value="none", inline=False)
+        embed.add_field(name="role",value="none",inline=False)
     else:
-      embed.add_field(name="role", value="none", inline=False)
+      embed.add_field(name="role",value="none",inline=False)
 
     await interaction.response.send_message(embed=embed)
   except Exception as e:
     print(e)
-    await interaction.response.send_message("エラーが発生しました", ephemeral=True)
+    await interaction.response.send_message("エラーが発生しました",ephemeral=True)
 
 keep_alive()
 client.run(os.getenv('token'))
